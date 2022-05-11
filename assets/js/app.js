185c3df3bd5d22462924389588ff31d6579a6427
kodak10 <atchinaymard10@gmail.com>
2022-05-11 12:23:36 +0000

    ok

--- assets/js/app.js
+++ assets/js/app.js
@@ -54,13 +54,13 @@ window.onscroll = function () {
     if (document.documentElement.scrollTop > 40) {
         /*au scrool*/
         document.getElementById("header").style.background = "#0e1532";
-        document.getElementsByClassName("navbar-toggler").style.background = "#fff !important";
+
         document.getElementById("navbar").style.padding = "10px 0px";
     }
     else {
         /*sans scrool*/
         document.getElementById("header").style.background = "transparent";
-        document.getElementsByClassName("navbar-toggler").style.background = "#fff !important";
+
         document.getElementById("header").style.padding = "10px 0px";
     }
 
